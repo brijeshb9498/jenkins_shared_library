e0029cc2f81c55f2ae9d8b998c8be037cac19ee4
Brijesh <134255343+brijeshb9498@users.noreply.github.com>
2023-09-30 19:48:24 +0530

    Update helm.groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,1 +1,5 @@
-
+def call(String chart_name = 'chart_k8s', 
+         String release_name = 'firstchart') {    
+        echo "This is helm.groovy"
+        def HelmInstall = helm install ${release_name} ${chart_name}
+        }    
